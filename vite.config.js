e9904441124d11358f5d915fd2ac6b5e9eec4c2d
jmdrajgupta <jmdrajkumargupta2002@gmail.com>
2024-11-27 08:08:41 +0530

    Configured project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//The vite.config.js file customizes the behavior of the Vite development server and build process for your project.
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecom/',
 })
